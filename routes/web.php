1a54d1d24c98d67b80044a19855ce7fe6cf2b968
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2026-06-10 16:47:39 +0700

    add migration web route hack

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
@@ -30,6 +31,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/jalankan-migrasi-rahasia', function () {
+    try {
+        // Menyuruh Laravel menjalankan php artisan migrate --force secara internal
+        Artisan::call('migrate', ['--force' => true]);
+        
+        return '<h1>Sihir Berhasil! ✨</h1><pre>' . Artisan::output() . '</pre>';
+    } catch (\Exception $e) {
+        return '<h1>Waduh Gagal:</h1><pre>' . $e->getMessage() . '</pre>';
+    }
 });
 
 require __DIR__.'/auth.php';
